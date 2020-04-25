878d6d29d865e20d0942c242411c85b5bdeef79b
lekegitrepo <lekedevrepo@gmail.com>
2020-04-25 07:38:21 -0500

    create ImageSlide component

--- src/carousel.js
+++ src/carousel.js
@@ -3,7 +3,9 @@ import React from 'react';
 class Carousel extends React.Component {
   render () {
     return (
-      <div className="carousel"></div>
+      <div className="carousel">
+        <ImageSlide url={ imgUrl } />
+      </div>
     );
   }
 }
